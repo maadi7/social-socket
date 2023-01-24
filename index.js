16480b340628d7d3966dc7cd5654dec5f30a2bf8
maadi <mishraadi110@gmail.com>
2023-01-24 22:12:28 +0530

    socket

---
+++ index.js
@@ -0,0 +1,44 @@
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "https://isocial.onrender.com",
+  },
+});
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //on user connection
+  console.log("a user connected");
+
+  //add user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+    console.log(users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", { senderId, text });
+  });
+
+  //on user disconnection
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
